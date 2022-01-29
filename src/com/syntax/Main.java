16feb1db519095d2727425cfc5910bc4370f9d8b
angelicazaporozhets <angiezap95@gmail.com>
2022-01-29 10:04:03 -0600

    basic structure is complete

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,5 +3,9 @@ package com.syntax;
 public class Main {
     public static void main(String[] args) {
         SolarSystem milkyWay = new SolarSystem();
+        milkyWay.sun = 1;
+        milkyWay.moon = 2;
+        milkyWay.planets = 3;
+
     }
 }
